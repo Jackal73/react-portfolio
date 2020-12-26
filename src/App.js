b307fff651606d9335c1fd8ab763b7e9fcec6038
shawn kebel <fxrender@outlook.com>
2020-12-26 01:37:20 -0600

    Create NavBar and add styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+import React from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from "./components/Home";
 import About from "./components/About";
 import SinglePost from "./components/SinglePost";
 import Post from "./components/Post";
 import Project from "./components/Project";
+import NavBar from "./components/NavBar";
+import Services from "./components/Services";
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar />
       <Switch>
         <Route component={Home} path='/' exact />
         <Route component={About} path='/about' />
         <Route component={SinglePost} path='/post/:slug' />
         <Route component={Post} path='/post' />
         <Route component={Project} path='/project' />
+        <Route component={Services} path='/services' />
       </Switch>
     </BrowserRouter>
   );
